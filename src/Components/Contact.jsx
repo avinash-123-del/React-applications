aaae6011b3f52e9d684092e63c29abdf6155e537
avinash-123-del <avichandraker@gmail.com>
2023-07-01 13:18:18 -0400

    building done

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div name='contact' className='bg-gradient-to-b from-black to-gray-800 w-full text-white pt-10 h-[100vh]'>
-            <div className='flex flex-col justify-center px-4 max-w-screen-lg mx-auto h-full'>
+        <div name='contact' className='bg-gradient-to-b from-black to-gray-800 w-full text-white pt-[50px]'>
+            <div className='flex flex-col justify-center px-4 max-w-screen-lg mx-auto h-screen'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
                     <p className='py-6'> Submit the form below to get in touch with me.</p>
